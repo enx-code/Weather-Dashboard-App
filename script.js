7abcfcb3ee7a0a655f1cbe3c7357c4a7d0ef00b2
Enky <enkh6at@yahoo.com>
2023-12-19 22:01:47 +0000

    forecast api weather done

--- script.js
+++ script.js
@@ -24,11 +24,19 @@ $(document).ready(function () {
           lon +
           "&appid=8b2153158617cd9459d1ae44448a1c63&units=metric";
 
+        var forecastURL =
+          "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+          lat +
+          "&lon=" +
+          lon +
+          "&appid=8b2153158617cd9459d1ae44448a1c63&units=metric";
+
         fetch(queryURL)
           .then(function (response) {
             return response.json();
           })
           .then(function (data) {
+            console.log(data.city.sunrise);
             console.log(data.city);
             var cityName = data.city.name;
             // var history = data.city
@@ -37,12 +45,25 @@ $(document).ready(function () {
 
             console.log(history);
           });
+
+        fetch(forecastURL)
+          .then(function (forecastResponse) {
+            return forecastResponse.json();
+          })
+          .then(function (forecastData) {
+            displayForecast(forecastData);
+          });
       });
   });
 
   function currentDay() {
-    const currentDay = dayjs().format("hh:mma, dddd, MMMM D, YYYY");
+    const currentDay = dayjs().format("hh:mma, D ddd, MMMM, YYYY");
     $("#today").text(currentDay);
   }
   currentDay();
+  function forecast() {
+    const tomorrow = dayjs().add(1, "day").format("hh:mma, D ddd, MMMM, YYYY");
+    $("#forecast").text(tomorrow);
+  }
+  forecast();
 });
